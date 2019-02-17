d7181020596ac8d9b6b442e7d5a5bca4d02b8738
Wandson <wandsonacop@hotmail.com>
2019-02-17 15:45:30 -0300

    Praticando o Design Pattern Strategy.

---
+++ Strategy/transportadora-com-strategy/src/com/algaworks/transportadora/service/frete/Normal.java
@@ -0,0 +1,10 @@
+package com.algaworks.transportadora.service.frete;
+
+import com.algaworks.transportadora.service.Frete;
+
+public class Normal implements Frete {
+
+	public double calcularPreco(int distancia) {
+		return distancia * 1.25 + 10;
+	}
+}
